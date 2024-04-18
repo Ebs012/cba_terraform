1759cf8f5ac6566eb90889afa23f93adf29dc796
username <ogundeleebunlomo@gmail.com>
2024-04-18 21:34:25 +0100

    my first commit

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_ami" {
+  default = ""
+}
+
+
+variable "vpc_id" {
+  default = ""
+}
+
+
+variable "key_name" {
+  default = ""
+}
